7651fb5361eff654f55fef99a6ea4c4d525b1de3
Patrick Magee <patrick.magee@live.co.uk>
2024-06-22 18:24:47 +0100

    dotnet-mage isn't as I expected.

--- dagger/main.go
+++ dagger/main.go
@@ -9,34 +9,45 @@ import (
 	"strings"
 )
 
-type HeroesProfileUploader struct{}
+type HeroesProfileUploader struct {
+}
 
-func (m *HeroesProfileUploader) Build(ctx context.Context, git *Directory) (string, error) {
+func (m *HeroesProfileUploader) Build(ctx context.Context, git *Directory) *dagger.Container {
 	return dag.Container().
 		From("mcr.microsoft.com/dotnet/sdk:8.0").
 		WithDirectory("/src", git, dagger.ContainerWithDirectoryOpts{
 			Exclude: []string{"**/bin/**", "**/obj/**"},
 		}).
 		WithWorkdir("/src").
-		WithExec([]string{
-			"dotnet",
-			"build"}).
-		Stdout(ctx)
+		WithExec([]string{"dotnet", "build", "-c", "Release"})
+}
+
+/*
+Well that sucks, it seems like mage is working on windows only. That means we need to build the clickonce application on windows.
+
+
+https://learn.microsoft.com/en-us/visualstudio/deployment/quickstart-deploy-using-clickonce-folder?view=vs-2022
+https://learn.microsoft.com/en-us/visualstudio/deployment/building-dotnet-clickonce-applications-from-the-command-line?view=vs-2022
+*/
+
+// dotnet publish ./Heroesprofile.Uploader.Windows/ --self-contained --os win -o ./publish
+// dotnet mage -al Heroesprofile.Uploader.exe -TargetDirectory publish
+// dotnet mage -new Application -t .\publish\Heroesprofile.Uploader.manifest -FromDirectory /publish -v 1.0.0.0 -IconFile ./Heroesprofile.Uploader.Windows/Resources/uploader_icon_dark.ico
+// dotnet mage -new Deployment -Install true -Publisher "Patrick Magee" -v 1.0.0.0 -AppManifest .\publish\Heroesprofile.Uploader.manifest -t Heroesprofile.Uploader.application
+
+func (m *HeroesProfileUploader) ClickOnce(ctx context.Context, git *Directory, version string) *dagger.Container {
+	return m.Publish(ctx, git, version).
+		WithExec([]string{"/root/.dotnet/tools/dotnet-mage", "-al", "Heroesprofile.Uploader.exe", "-TargetDirectory", "/publish"}).
+		WithExec([]string{"/root/.dotnet/tools/dotnet-mage", "-new", "Application", "-t", "/publish/Heroesprofile.Uploader.manifest", "-FromDirectory", "/publish", "-v", version, "-IconFile", "/publish/Resources/uploader_icon_dark.ico"}).
+		WithExec([]string{"/root/.dotnet/tools/dotnet-mage", "-new", "Deployment", "-Install", "true", "-Publisher", "Patrick Magee", "-v", version, "-AppManifest", "/publish/Heroesprofile.Uploader.manifest", "-t", "Heroesprofile.Uploader.application"})
 }
 
 func (m *HeroesProfileUploader) Publish(ctx context.Context, git *Directory, version string) *dagger.Container {
-	return dag.Container().
-		From("mcr.microsoft.com/dotnet/sdk:8.0").
-		WithDirectory("/src", git, dagger.ContainerWithDirectoryOpts{
-			Exclude: []string{"**/bin/**", "**/obj/**"},
-		}).
-		WithWorkdir("/src/HeroesProfile.Uploader.Windows").
-		WithExec([]string{
-			"msbuild",
-			"/t:publish",
-			fmt.Sprintf("/property:ApplicationVersion=%s", version),
-			"/p:PublishProfile=Properties/PublishProfiles/ClickOnceProfile.pubxml",
-			"/p:PublishDir=\"/publish\""})
+	return m.Build(ctx, git).
+		WithWorkdir("/src").
+		WithExec([]string{"dotnet", "tool", "install", "microsoft.dotnet.mage", "--global", "--version", "8.0.0"}).
+		WithExec([]string{"mkdir", "/publish"}).
+		WithExec([]string{"dotnet", "publish", "Heroesprofile.Uploader.Windows", "--self-contained", "--os", "win", "-o", "/publish"})
 }
 
 func (m *HeroesProfileUploader) Release(
@@ -47,13 +58,17 @@ func (m *HeroesProfileUploader) Release(
 
 	version := strings.TrimPrefix(tag, "v")
 	publish := m.Publish(ctx, git.Directory("src"), version)
-	fileNames, _ := publish.Directory("/publish").Entries(ctx)
+	assets := publish.Directory("/publish")
 
-	files := []*dagger.File{}
+	// fileNames, _ := assets.Entries(ctx)
 
-	for _, file := range fileNames {
-		files = append(files, publish.File(file))
-	}
+	// files := []*dagger.File{}
+
+	// for _, file := range fileNames {
+	// 	files = append(files, publish.File(file))
+	// }
+
+	zip := dag.Arc().ArchiveDirectory(fmt.Sprintf("HeroesProfile.Uploader_%s_windows_amd64", tag), assets).Zip()
 
 	gh := dag.Gh(dagger.GhOpts{
 		Token:  token,
@@ -62,8 +77,8 @@ func (m *HeroesProfileUploader) Release(
 	})
 
 	_, err := gh.Release().Create(ctx, tag, "", dagger.GhReleaseCreateOpts{
-		Target:        "master",
-		Files:         files,
+		Target:        "dotnet-upgrade",
+		Files:         []*dagger.File{zip},
 		Latest:        true,
 		VerifyTag:     true,
 		Draft:         true,
